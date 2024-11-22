381af34e7d96a95d4b908d2ae34ab5b08f7d9646
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2024-11-22 15:29:22 +0600

    DB Table [Update]

---
+++ Domain/Models/ChatDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Models;
+
+public partial class ChatDbContext : DbContext
+{
+    public ChatDbContext()
+    {
+    }
+
+    public ChatDbContext(DbContextOptions<ChatDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblUserInformation> TblUserInformations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ChatDB;Username=postgres;Password=1234");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblUserInformation>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("tbluserinformation_pkey");
+
+            entity.ToTable("TblUserInformation");
+
+            entity.Property(e => e.UserId).HasDefaultValueSql("nextval('tbluserinformation_userid_seq'::regclass)");
+            entity.Property(e => e.ContactNumber).HasMaxLength(15);
+            entity.Property(e => e.ContactNumberCountryCode).HasMaxLength(3);
+            entity.Property(e => e.CreationDateTime).HasColumnType("timestamp without time zone");
+            entity.Property(e => e.Email).HasMaxLength(320);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastModifiedDateTime).HasColumnType("timestamp without time zone");
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+            entity.Property(e => e.Title).HasMaxLength(5);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Domain/Models/TblUserInformation.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Models;
+
+public partial class TblUserInformation
+{
+    public long UserId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public DateOnly Dob { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string ContactNumberCountryCode { get; set; } = null!;
+
+    public string ContactNumber { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreationDateTime { get; set; }
+
+    public DateTime LastModifiedDateTime { get; set; }
+}
