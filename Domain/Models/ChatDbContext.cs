de6d1be1a189073797e04d2a92fde8aef1c9770e
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2024-11-22 12:32:59 +0600

    DataBase Scaffolding [Done]

---
+++ Domain/Models/ChatDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Models;
+
+public partial class ChatDbContext : DbContext
+{
+    public ChatDbContext()
+    {
+    }
+
+    public ChatDbContext(DbContextOptions<ChatDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tbluserinformation> Tbluserinformations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ChatDB;Username=postgres;Password=1234");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tbluserinformation>(entity =>
+        {
+            entity.HasKey(e => e.Userid).HasName("tbluserinformation_pkey");
+
+            entity.ToTable("tbluserinformation");
+
+            entity.Property(e => e.Userid).HasColumnName("userid");
+            entity.Property(e => e.Contactnumber)
+                .HasMaxLength(15)
+                .HasColumnName("contactnumber");
+            entity.Property(e => e.Contactnumbercountrycode)
+                .HasMaxLength(3)
+                .HasColumnName("contactnumbercountrycode");
+            entity.Property(e => e.Creationdatetime).HasColumnName("creationdatetime");
+            entity.Property(e => e.Dob).HasColumnName("dob");
+            entity.Property(e => e.Email)
+                .HasMaxLength(320)
+                .HasColumnName("email");
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(50)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Isactive).HasColumnName("isactive");
+            entity.Property(e => e.Lastmodifieddate).HasColumnName("lastmodifieddate");
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(50)
+                .HasColumnName("lastname");
+            entity.Property(e => e.Middlename)
+                .HasMaxLength(50)
+                .HasColumnName("middlename");
+            entity.Property(e => e.Title)
+                .HasMaxLength(5)
+                .HasColumnName("title");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Domain/Models/Tbluserinformation.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Models;
+
+public partial class Tbluserinformation
+{
+    public long Userid { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string? Middlename { get; set; }
+
+    public string Lastname { get; set; } = null!;
+
+    public DateOnly Dob { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Contactnumbercountrycode { get; set; } = null!;
+
+    public string Contactnumber { get; set; } = null!;
+
+    public bool Isactive { get; set; }
+
+    public DateOnly Creationdatetime { get; set; }
+
+    public DateOnly Lastmodifieddate { get; set; }
+}
